6a2b39e5f9f55f197a707e89f20b40629b832589
Mike Walters <mike@eventbooking.com>
2016-03-11 11:27:19 -0500

    removed old code

--- demo/controller.js
+++ demo/controller.js
@@ -5,24 +5,6 @@ function TestController() {
     this.lastWeek = moment().subtract(8,'d');
     this.hoursAgo = moment().subtract(3,'h');
     this.daysAgo = moment().subtract(24,'h');
-    this.decimalMoney = 100.50;
-    this.codeMoney = 'GBP';
-    this.moneyObject = {
-        amount: 123.45,
-        currency: 'EUR'
-    }
-    this.moneyObject1k = {
-        amount: 1234.45,
-        currency: 'EUR'
-    }
-    this.moneyObject1m = {
-        amount: 1235678.45,
-        currency: 'EUR'
-    }
-    this.moneyObjectAt1m = {
-        amount: 1000000.00,
-        currency: 'EUR'
-    }
 }
 
 angular.module("demo").controller("testcontroller", TestController);
