158e7fa32a1314c5081efee41b8777ae96497e89
Mike Walters <mike@eventbooking.com>
2018-02-12 13:24:25 -0500

    updating packages

--- src/moment.d.ts
+++ src/moment.d.ts
@@ -1,6 +1,6 @@
 // see: https://github.com/moment/moment/issues/3763#issuecomment-306630482
 
 declare var moment: any;
-import * as _moment from 'twix';
+import * as _moment from 'moment';
 export as namespace moment;
 export = _moment;
